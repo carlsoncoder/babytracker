ca178397ec3e0d56018303b00ab7e896d6a34c3a
Justin Carlson <jcarlson1985420@gmail.com>
2015-07-28 09:35:37 -0500

    Missed one bug with overall length calculations

--- public/js/babytracker-factories.js
+++ public/js/babytracker-factories.js
@@ -67,7 +67,7 @@ babyTrackerFactories.factory('feedings', ['$http', function($http) {
 
                         var leftLength = isNullOrUndefined(feeding.lengthInMinutesLeft) ? 0 : feeding.lengthInMinutesLeft;
                         var rightLength = isNullOrUndefined(feeding.lengthInMinutesRight) ? 0 : feeding.lengthInMinutesRight;
-                        feeding.overallLength = leftLength + rightLength;
+                        feeding.overallLength = Math.round(leftLength + rightLength);
 
                         feeding.lengthInMinutesLeft = Math.round(feeding.lengthInMinutesLeft);
                         feeding.lengthInMinutesRight = Math.round(feeding.lengthInMinutesRight);
